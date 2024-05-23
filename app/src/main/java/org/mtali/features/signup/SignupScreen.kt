f2857d4d1e531f6dee5d5bc71c96c57294a51b19
mtali <emmanuelmtali@yahoo.com>
2024-05-23 23:33:46 +0300

    Implement ui for signup

--- app/src/main/java/org/mtali/features/signup/SignupScreen.kt
+++ app/src/main/java/org/mtali/features/signup/SignupScreen.kt
@@ -1,24 +1,137 @@
 package org.mtali.features.signup
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.mtali.R
+import org.mtali.core.designsystem.components.boltHeader
+import org.mtali.core.designsystem.components.emailField
+import org.mtali.core.designsystem.components.height
+import org.mtali.core.designsystem.components.passwordField
+import org.mtali.core.designsystem.components.primaryButton
+import org.mtali.core.utils.ELEMENT_WIDTH
+import org.mtali.core.utils.capitalizeWords
+import org.mtali.core.utils.handleToast
 
 
 @Composable
-fun SignupRoute() {
-    SignupScreen()
+fun SignupRoute(viewModel: SignupViewModel = hiltViewModel()) {
+
+    val context = LocalContext.current
+    viewModel.toastHandler = {
+        context.handleToast(it)
+    }
+
+    val form by viewModel.form
+
+    val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+
+    SignupScreen(
+        form = form,
+        isLoading = isLoading,
+        onEmailChange = viewModel::onEmailChange,
+        onPasswordChange = viewModel::onPasswordChange,
+        onAttemptSignup = viewModel::onAttemptSignup,
+        onNameChange = viewModel::onNameChange,
+        onClose = {}
+    )
 }
 
 @Composable
-private fun SignupScreen() {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        item {
-            Text(text = "Signup screen")
+private fun SignupScreen(
+    form: SignupForm,
+    isLoading: Boolean,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onAttemptSignup: () -> Unit,
+    onNameChange: (String) -> Unit,
+    onClose: () -> Unit
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        IconButton(onClick = onClose) {
+            Icon(imageVector = Icons.Filled.Close, contentDescription = "close")
+        }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            height(64.dp)
+
+            boltHeader(subtitle = R.string.need_ride)
+
+            height(30.dp)
+
+            nameField(
+                modifier = Modifier.width(ELEMENT_WIDTH),
+                name = form.name,
+                onNameChange = onNameChange
+            )
+
+            emailField(
+                modifier = Modifier.width(ELEMENT_WIDTH),
+                email = form.email,
+                onEmailChange = onEmailChange
+            )
+
+            height(6.dp)
+
+            passwordField(
+                modifier = Modifier.width(ELEMENT_WIDTH),
+                password = form.password,
+                onPasswordChange = onPasswordChange
+            )
+
+            height(12.dp)
+
+            primaryButton(
+                onClick = onAttemptSignup,
+                modifier = Modifier.width(ELEMENT_WIDTH),
+                isLoading = isLoading
+            )
+
+            height(25.dp)
+
         }
     }
+}
+
+private fun LazyListScope.nameField(
+    modifier: Modifier = Modifier,
+    name: String,
+    onNameChange: (String) -> Unit
+) = item {
+    OutlinedTextField(
+        modifier = modifier,
+        value = name,
+        onValueChange = {
+            onNameChange(it.capitalizeWords())
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        label = {
+            Text(text = stringResource(id = R.string.name))
+        }
+    )
 }
---
+++ app/src/main/java/org/mtali/features/signup/SignupViewModel.kt
@@ -0,0 +1,41 @@
+package org.mtali.features.signup
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.mtali.core.models.ToastMessage
+import org.mtali.core.utils.update
+import javax.inject.Inject
+
+@HiltViewModel
+class SignupViewModel @Inject constructor() : ViewModel() {
+
+    private val _form = mutableStateOf(SignupForm())
+    val form: State<SignupForm> = _form
+
+    var toastHandler: ((ToastMessage) -> Unit)? = null
+
+    private var loginJob: Job? = null
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    fun onAttemptSignup() {
+
+    }
+
+    fun onEmailChange(email: String) = _form.update { it.copy(email = email) }
+
+    fun onPasswordChange(password: String) = _form.update { it.copy(password = password) }
+
+    fun onNameChange(name: String) = _form.update { it.copy(name = name) }
+
+
+}
+
+data class SignupForm(val name: String = "", val email: String = "", val password: String = "")
