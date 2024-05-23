f3a4bbe848c9031dfa8b94bb1daa9b9bef1fb634
mtali <emmanuelmtali@yahoo.com>
2024-05-23 22:58:55 +0300

    Move common form fields to reusable components

---
+++ app/src/main/java/org/mtali/core/designsystem/components/CommonFieds.kt
@@ -0,0 +1,92 @@
+package org.mtali.core.designsystem.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import org.mtali.R
+
+fun LazyListScope.emailField(
+    modifier: Modifier = Modifier,
+    email: String,
+    onEmailChange: (String) -> Unit
+) = item {
+    OutlinedTextField(
+        modifier = modifier,
+        value = email,
+        onValueChange = onEmailChange,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        label = {
+            Text(text = stringResource(id = R.string.email))
+        }
+    )
+}
+
+fun LazyListScope.passwordField(
+    modifier: Modifier = Modifier,
+    password: String,
+    onPasswordChange: (String) -> Unit
+) = item {
+
+    var showPassword by rememberSaveable { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = password,
+        onValueChange = onPasswordChange,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+        label = {
+            Text(text = stringResource(id = R.string.password))
+        },
+        trailingIcon = {
+            val image = if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+            IconButton(onClick = { showPassword = !showPassword }) {
+                Icon(imageVector = image, contentDescription = null)
+            }
+        }
+    )
+}
+
+fun LazyListScope.primaryButton(
+    modifier: Modifier = Modifier,
+    @StringRes title: Int = R.string.cont,
+    isLoading: Boolean,
+    onClick: () -> Unit,
+) = item {
+    Button(
+        modifier = modifier,
+        onClick = { if (!isLoading) onClick() },
+        shape = OutlinedTextFieldDefaults.shape
+    ) {
+        if (!isLoading)
+            Text(text = stringResource(id = title))
+        else
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier.size(25.dp),
+                strokeWidth = 1.5.dp
+            )
+    }
+}
--- app/src/main/java/org/mtali/features/login/LoginScreen.kt
+++ app/src/main/java/org/mtali/features/login/LoginScreen.kt
@@ -2,37 +2,20 @@ package org.mtali.features.login
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
@@ -41,7 +24,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.mtali.R
 import org.mtali.core.designsystem.components.boltHeader
+import org.mtali.core.designsystem.components.emailField
 import org.mtali.core.designsystem.components.height
+import org.mtali.core.designsystem.components.passwordField
+import org.mtali.core.designsystem.components.primaryButton
 import org.mtali.core.utils.ELEMENT_WIDTH
 import org.mtali.core.utils.handleToast
 
@@ -108,10 +94,10 @@ private fun LoginScreen(
 
         height(12.dp)
 
-        loginButton(
+        primaryButton(
             onClick = onAttemptLogin,
             modifier = Modifier.width(ELEMENT_WIDTH),
-            isLoading = isLoading
+            isLoading = isLoading,
         )
 
         height(25.dp)
@@ -120,70 +106,7 @@ private fun LoginScreen(
     }
 }
 
-private fun LazyListScope.emailField(
-    modifier: Modifier = Modifier,
-    email: String,
-    onEmailChange: (String) -> Unit
-) = item {
-    OutlinedTextField(
-        modifier = modifier,
-        value = email,
-        onValueChange = onEmailChange,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-        label = {
-            Text(text = stringResource(id = R.string.email))
-        }
-    )
-}
 
-private fun LazyListScope.passwordField(
-    modifier: Modifier = Modifier,
-    password: String,
-    onPasswordChange: (String) -> Unit
-) = item {
-
-    var showPassword by rememberSaveable { mutableStateOf(false) }
-
-    OutlinedTextField(
-        modifier = modifier,
-        value = password,
-        onValueChange = onPasswordChange,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
-        label = {
-            Text(text = stringResource(id = R.string.password))
-        },
-        trailingIcon = {
-            val image = if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-            IconButton(onClick = { showPassword = !showPassword }) {
-                Icon(imageVector = image, contentDescription = null)
-            }
-        }
-    )
-}
-
-private fun LazyListScope.loginButton(
-    modifier: Modifier = Modifier,
-    isLoading: Boolean,
-    onClick: () -> Unit
-) = item {
-    Button(
-        modifier = modifier,
-        onClick = {
-            if (!isLoading) onClick()
-        },
-        shape = OutlinedTextFieldDefaults.shape
-    ) {
-        if (!isLoading)
-            Text(text = stringResource(id = R.string.cont))
-        else
-            CircularProgressIndicator(
-                color = MaterialTheme.colorScheme.onPrimary,
-                modifier = Modifier.size(25.dp),
-                strokeWidth = 1.5.dp
-            )
-    }
-}
 
 private fun LazyListScope.signupText(
     modifier: Modifier = Modifier,
