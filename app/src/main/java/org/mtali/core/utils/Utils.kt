bc78b2468b145b088041bcb2299a7e90ce17e440
mtali <emmanuelmtali@yahoo.com>
2024-05-23 17:05:26 +0300

    Add utility to prevent multiple launches 🏃

--- app/src/main/java/org/mtali/core/utils/Utils.kt
+++ app/src/main/java/org/mtali/core/utils/Utils.kt
@@ -2,10 +2,26 @@ package org.mtali.core.utils
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 
 val ELEMENT_WIDTH = 290.dp
 
 inline fun <T> MutableState<T>.update(block: (T) -> T) {
     value = block(value)
+}
+
+fun CoroutineScope.preventMultipleLaunches(
+    mutex: Mutex,
+    operation: suspend () -> Unit
+) {
+    launch {
+        if (mutex.isLocked) return@launch
+        mutex.withLock {
+            operation()
+        }
+    }
 }
