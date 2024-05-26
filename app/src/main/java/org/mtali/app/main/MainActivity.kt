d0df0f6c3f02354d954b15153c47bdd703de0cea
mtali <emmanuelmtali@yahoo.com>
2024-05-26 20:06:50 +0300

    Install splashscreen

--- app/src/main/java/org/mtali/app/main/MainActivity.kt
+++ app/src/main/java/org/mtali/app/main/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -25,6 +26,7 @@ class MainActivity : ComponentActivity() {
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
@@ -36,6 +38,8 @@ class MainActivity : ComponentActivity() {
                     .collect()
             }
         }
+
+        splashScreen.setKeepOnScreenCondition { uiState !is MainUiState.Success }
 
         setContent {
             val appState = rememberBoltAppState()
