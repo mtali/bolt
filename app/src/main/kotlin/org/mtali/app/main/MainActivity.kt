e2ee51f2cc8c9cc6f21550942234f5ddae6f74c0
mtali <emmanuelmtali@yahoo.com>
2024-05-31 17:31:48 +0300

    Woopsie!

--- app/src/main/kotlin/org/mtali/app/main/MainActivity.kt
+++ app/src/main/kotlin/org/mtali/app/main/MainActivity.kt
@@ -16,7 +16,6 @@
 package org.mtali.app.main
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -25,7 +24,6 @@ import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -72,13 +70,5 @@ class MainActivity : ComponentActivity() {
         )
       }
     }
-  }
-
-  private fun requestPermission() {
-    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-    }
-  }
-
-  private fun requestLocation() {
   }
 }
